a8cb01564ca63ba00c4d751a2bfe3864fa9be9a5
JamieKessler <jkessler5535@gmail.com>
2025-03-02 15:39:07 -0800

    created header

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import './App.css'
-import Header from "./components/header";
+import Header from "./components/header/header";
 
 function App() {
   return (
-    <>
+    <div className="container">
       <Header/>
     
-    </>
+    </div>
   )
 }
 
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import "./header.css";
 
 
 const Header = () => {
   return (
     <header className="header">
       <h3>Web Crafters</h3>
-      <nav>
+      <nav className="nav-bar">
         <ul>
           <li><a href="#home">Home</a></li>
           <li><a href="#about">About</a></li>
