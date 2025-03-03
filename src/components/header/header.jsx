7a71e89c303a32da6a04b842733f3829c0589925
JamieKessler <jkessler5535@gmail.com>
2025-03-02 17:34:56 -0800

    added btn component styles

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,29 +1,26 @@
 import React from "react";
 import "./header.css";
+import SignUpBtn from "../sign-up-btn/sign-up-btn.jsx";
 
 
 
-const Header = () => {
+function Header() {
   return (
-    <header className="header">
-      <h3>Web Crafters</h3>
+    <header>
       <nav className="nav-bar">
+        <h3>Web Crafters</h3>
         <ul>
           <li><a href="#home">Home</a></li>
           <li><a href="#about">About</a></li>
-          <li><a href="#services">Services</a></li> 
+          <li><a href="#services">Services</a></li>
           <li><a href="#contact">Contact</a></li>
+          <li><a href="#home">Log in</a></li>
         </ul>
-        <ul>
-          <li><a href="#home">Log n</a></li>
-          <li><a href="#home"></a>Contact sales</li>
-        </ul>
-
-
+        <SignUpBtn />
       </nav>
     </header>
   );
-};
+}
 
 export default Header;  
 
