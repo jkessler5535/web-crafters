c31c504f9c19d1b74ffd5837186c3026c8bf0baa
JamieKessler <jkessler5535@gmail.com>
2025-03-26 16:57:11 -0700

    added hero page

---
+++ src/components/hero-page/hero.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./hero.css";
+
+
+function Hero() {
+  return (
+    <div className="hero">
+      <div className="bob-the-blob"></div>
+    </div>
+  );
+}
+
+export default Hero;
