e30bb9547548f84b0e8e1ae8aa101c85a75bf8e3
JamieKessler <jkessler5535@gmail.com>
2025-04-12 13:53:59 -0700

    added hero.js component, added intro content

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import './App.css'
 import Header from "./components/header/header";
+import Hero from "./components/hero-page/hero";
 
 function App() {
   return (
     <div className="container">
       <Header/>
+      <Hero/>
     
     </div>
   )
--- src/components/hero-page/hero.jsx
+++ src/components/hero-page/hero.jsx
@@ -1,13 +1,15 @@
-import React from "react";
+import React from 'react';
 import "./hero.css";
 
 
 function Hero() {
-  return (
-    <div className="hero">
-      <div className="bob-the-blob"></div>
-    </div>
-  );
+    return (
+        <div className="hero">
+            <h3>More Than a Website Builder</h3>
+            <h2>Transform Your Vision Into a Stunning Website</h2>
+            <p>Webflow is a pioneering platform that empowers marketers, designers, and developers to collaboratively create, manage, and refine high-impact web experiences — all in one place.</p>
+        </div>
+    );
 }
 
 export default Hero;
