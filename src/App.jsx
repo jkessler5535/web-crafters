346c6ce5bb3045d870f5443902b73ea18c6c043b
JamieKessler <jkessler5535@gmail.com>
2025-04-19 20:22:10 -0700

    made changes to hamburger menu on tablet/mobile

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 import './App.css'
+import './index.css';
+
+
+//Components
 import Header from "./components/header/header";
 import Hero from "./components/hero-page/hero";
 import Intro from "./components/intro/intro";
--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -12,25 +12,25 @@ import ScaleIcon from '../../assets/about-icons/scale.jsx';
 function About() {
     return (
         <div className="about-section">
-            <div className="block 1">
+            <div className="block">
             <CursorIcon/>
               <h2>Design & Build</h2>
               <p>Take full creative control.
               Designers can dive into HTML, CSS, and JavaScript visually, while marketers build with reusable, brand-approved components—no code required.</p>
             </div>
-            <div className="block 2">
+            <div className="block">
               <RocketIcon/> 
             <h2>Publish & Edit</h2>
               <p>Update content your way.
               Use Webflow’s CMS to manage content visually in the Designer or automate it through powerful headless APIs—perfect for scaling teams.</p>
             </div>
-            <div className="block 3"> 
+            <div className="block"> 
               <OptiIcon/>
             <h2>Analyze & Optimize</h2>
               <p>Turn your site into a growth engine.
               Unlock built-in tools for AI personalization, A/B testing, SEO, localization, and more—all designed to help you convert and scale.</p>
             </div>
-            <div className="block 4"> 
+            <div className="block"> 
               <ScaleIcon/>
               <h2>Scale & Collaborate</h2>
               <p>Build for today, grow for tomorrow.
