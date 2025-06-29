6aab64560c8bf8082986a2cb86a9c37c5a05a33c
JamieKessler <jkessler5535@gmail.com>
2025-06-29 16:12:12 -0700

    chaged size of image in tablet size for media query/changes to h1 in media query in hero.js

--- src/components/hero-page/hero.jsx
+++ src/components/hero-page/hero.jsx
@@ -16,7 +16,10 @@ function Hero() {
             <h3>More Than a Website Builder</h3>
             <h1>Transform Your Vision Into a Stunning Website</h1>
             <p>Webflow is a pioneering platform that empowers marketers, designers, and developers to collaboratively create, manage, and refine high-impact web experiences — all in one place.</p>
-            <BuildBtn/> 
+            <div className="hero-btns">
+                <BuildBtn/> 
+                <a href="#" id="contact">Contact Sales</a>
+            </div>
         </div>
     );
 }
