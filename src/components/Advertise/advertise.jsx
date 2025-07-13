64689e5d9699ac88927eb9f7f049801357547996
JamieKessler <jkessler5535@gmail.com>
2025-07-12 19:10:35 -0700

    took the orb off/adjusted the diamond position

--- src/components/Advertise/advertise.jsx
+++ src/components/Advertise/advertise.jsx
@@ -3,8 +3,9 @@ import './advertise.css';
 function Advertise() {
     return (
         <div className="advertise-container">
-          <div className="orb"></div>
             <div className="diamond-advertise"></div>
+            <h2>AI at Web Crafters</h2>
+            <p></p>
         </div>
     );
 }
