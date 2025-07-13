b774818ce0378343786e2e70903d0cc2e7f010cb
JamieKessler <jkessler5535@gmail.com>
2025-07-13 13:55:36 -0700

    added padding to intro.js

--- src/components/intro/intro.jsx
+++ src/components/intro/intro.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import LottieAnimation from '../Lottie-Animation/lottie-comp';
 import './intro.css';
 
@@ -7,15 +6,15 @@ import './intro.css';
 function Intro() {
     return (
         <div className="intro">
-          <div className="intro-left">
-            <h1>Launch Pixel-
-              perfect sites
-            </h1>
-            <p>Rethink the web dev cycle with Webflow. Give your design and marketing teams the power to launch sophisticated sites quickly — so your dev team can focus on more complex work, not pixel-perfect revisions.</p>
-          </div>
-          <div className="intro-right">
-            <LottieAnimation/>
-          </div>
+            <div className="intro-left">
+              <h1>Launch Pixel-
+                perfect sites
+              </h1>
+              <p>Rethink the web dev cycle with Webflow. Give your design and marketing teams the power to launch sophisticated sites quickly — so your dev team can focus on more complex work, not pixel-perfect revisions.</p>
+            </div>
+            <div className="intro-right">
+              <LottieAnimation/>
+            </div>
         </div>
     );
 }
