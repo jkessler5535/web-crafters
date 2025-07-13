2b118789da3ed6a1219a903046a63324e2b28792
JamieKessler <jkessler5535@gmail.com>
2025-07-12 19:39:01 -0700

    added AI advertisement to page

--- src/components/Advertise/advertise.jsx
+++ src/components/Advertise/advertise.jsx
@@ -4,8 +4,8 @@ function Advertise() {
     return (
         <div className="advertise-container">
             <div className="diamond-advertise"></div>
-            <h2>AI at Web Crafters</h2>
-            <p></p>
+              <h2>AI at Web Crafters</h2>
+              <p>Elevate your site with AI embedded across the Web Crafters platform and extend its power with deep integrations with a growing ecosystem of AI-powered apps.</p>
         </div>
     );
 }
