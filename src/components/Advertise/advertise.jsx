b2458307cd49d761ce851e70e375a0a989e985f8
JamieKessler <jkessler5535@gmail.com>
2025-07-13 13:11:38 -0700

    changed layout on hero. added .content-wrapper and added margin to main elements for smoother look

--- src/components/Advertise/advertise.jsx
+++ src/components/Advertise/advertise.jsx
@@ -3,7 +3,8 @@ import './advertise.css';
 function Advertise() {
     return (
         <div className="advertise-container">
-            <div className="diamond-advertise"></div>
+            <div 
+            className="diamond-advertise"></div>
               <h2>AI at Web Crafters</h2>
               <p>Elevate your site with AI embedded across the Web Crafters platform and extend its power with deep integrations with a growing ecosystem of AI-powered apps.</p>
         </div>
--- src/components/hero-page/hero.jsx
+++ src/components/hero-page/hero.jsx
@@ -11,12 +11,14 @@ import BuildBtn from '../start-build-btn/build-btn.jsx';
 function Hero() {
     return (
         <div className="hero">
-            <h3>More Than a Website Builder</h3>
-            <h1>Transform Your Vision Into a Stunning Website</h1>
-            <p>Webflow is a pioneering platform that empowers marketers, designers, and developers to collaboratively create, manage, and refine high-impact web experiences — all in one place.</p>
-            <div className="hero-btns">
-                <BuildBtn/> 
-                <a href="#" id="contact">Contact Sales</a>
+            <div className="hero-content">
+                <h3>More Than a Website Builder</h3>
+                <h1>Transform Your Vision Into a Stunning Website</h1>
+                <p>Webflow is a pioneering platform that empowers marketers, designers, and developers to collaboratively create, manage, and refine high-impact web experiences — all in one place.</p>
+                <div className="hero-btns">
+                    <BuildBtn/> 
+                    <a href="#" id="contact">Contact Sales</a>
+                </div>
             </div>
         </div>
     );
